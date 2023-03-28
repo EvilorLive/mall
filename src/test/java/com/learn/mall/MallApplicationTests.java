1690e906750047d6d485cce44cf984943fa53f39
EvilorLive <1291271054@qq.com>
2023-03-28 11:09:38 +0800

    first commit

---
+++ src/test/java/com/learn/mall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.mall;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
